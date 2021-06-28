ec82c8929811d5cc042f3568627e61f0908c1770
LuuNV <luunv@HBLABs-Mac-mini.local>
2021-06-28 12:01:57 +0700

    feat: [#6] add badge

--- src/components/atoms/atoms.stories.tsx
+++ src/components/atoms/atoms.stories.tsx
@@ -4,13 +4,21 @@ import {View} from 'react-native';
 import {storiesOf} from '@storybook/react-native';
 import {Icon, Button} from './';
 import {images, colors} from '../../themes';
+import Badge from './Badge';
 
 const stories = storiesOf('Atoms', module);
-const baseStyle = {marginTop: 10, marginLeft: 10, marginRight: 10};
 
+const baseStyle = {marginTop: 10, marginLeft: 10, marginRight: 10}
 stories.add('Icon', () => (
   <View>
     <Icon style={baseStyle} source={images.icUser} />
+  </View>
+));
+
+stories.add('Badge', () => (
+  <View>
+    <Badge number={2} badgeStyle={baseStyle} />
+    <Badge number={10} badgeStyle={baseStyle} />
   </View>
 ));
 
